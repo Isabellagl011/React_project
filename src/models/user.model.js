77a5ef227a2bb6afc03d22a1eb26879cdfb1d144
Isabella <isabella.grajalesl@autonoma.edu.co>
2022-04-24 00:16:52 -0500

    Finalización react-project

---
+++ src/models/user.model.js
@@ -0,0 +1,24 @@
+const user = {
+  name: {
+    type: String,
+    require: true,
+    min: 3,
+  },
+  lastname: {
+    type: String,
+    require: true,
+    min: 3,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+    unique: true,
+  },
+  active: {
+    type: Boolean,
+    require: true,
+  },
+};
